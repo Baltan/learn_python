912e3251af7d46d6c9741a9ecd76c82397462025
Baltan <617640006@qq.com>
2019-05-14 16:59:57 +0800

    for语句

---
+++ TestFor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+for animal in ["dog", "cat", "mouse"]:
+    print("{} is a mammal".format(animal))
+
+print("------------------------------这是一条分割线------------------------------")
+
+sum = 0
+
+for x in range(5, 10):
+    sum += x
+
+print("sum:", sum)
